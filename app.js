ec8f4d9b5e2890636ce288eaf9a9d441784a4868
prdp99 <iampradippariyar@gmail.com>
2022-09-03 22:21:54 +0545

    first commit

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.get("/", (req, res) => {
   res.send("it's running");
 });
 const port = process.env.PORT || 5000;
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.jyo3d.mongodb.net/brandnewtable?retryWrites=true&w=majority`
