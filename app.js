a782f1d0c87cdbceb2c7887ace7f68e24571488b
prdp99 <iampradippariyar@gmail.com>
2022-09-03 15:27:03 +0545

    first commit

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "*" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
+app.get("/", (req, res) => {
+  res.send("it's running");
+});
 const port = process.env.PORT || 5000;
 mongoose
   .connect(
